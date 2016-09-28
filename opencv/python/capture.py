5b59f237e197622272e6fee4bb9dec3b6f009c0f
simondlevy <simon.d.levy@gmail.com>
2016-09-28 16:53:09 -0400

    Started Python implementation

---
+++ opencv/python/capture.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+'''
+m021capture.py : capture frames from Leopard Imaging LI-USB30-M021 camera and display them using OpenCV
+
+Copyright (C) 2016 Simon D. Levy
+
+This file is part of M021_V4L2.
+
+M021_V4L2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+BreezySTM32 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with M021_V4L2.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import cv2
+from m021v4l2 import Capture1280x720
+from time import time
+
+cap = Capture1280x720()
+
+start = time()
+
+count = 0
+
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    count += 1
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+elapsed = time() - start
+print('%d frames in %3.2f seconds = %3.2f fps' % (count, elapsed, count/elapsed))
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ opencv/python/m021v4l2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Capture1280x720:
+
+    def __init__(self):
+
+        return
+
+    def read(self):
+
+        return True, np.zeros((720,1280,3))
+
+    def release(self):
+
+        return
