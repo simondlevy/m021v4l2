6c9c741943745535902805f80872c20d3c9af66c
simondlevy <simon.d.levy@gmail.com>
2016-09-28 20:13:26 -0400

    More progress on Python extension

--- opencv/python/example.py
+++ opencv/python/example.py
@@ -3,8 +3,8 @@
 import libm021v4l2
 import numpy as np
 
-a = np.zeros((8, 7, 3, 2))
-b = np.array([[1.1, 2.2, 3.3], [1.2, 1.3, 1.4]])
 c = np.array([[1.1, 2.2, 3.3], [1.2, 1.3, 1.4]])
-libm021v4l2.example(a, b, c)
+
+libm021v4l2.acquire(c)
+
 print(c)
--- opencv/python/libm021v4l2.c
+++ opencv/python/libm021v4l2.c
@@ -1,34 +1,17 @@
 #include "Python.h"
 #include "numpy/arrayobject.h"
 
-#include <stdio.h>
-
-static PyObject* example (PyObject *dummy, PyObject *args)
+static PyObject* acquire (PyObject *dummy, PyObject *args)
 {
-    PyObject *arg1=NULL, *arg2=NULL, *out=NULL;
-    PyArrayObject *arr1=NULL, *arr2=NULL, *oarr=NULL;
+    PyObject *out=NULL;
+    PyArrayObject *oarr=NULL;
 
-    if (!PyArg_ParseTuple(args, "OOO!", &arg1, &arg2,
-                &PyArray_Type, &out)) return NULL;
+    if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &out)) return NULL;
 
-    arr1 = (PyArrayObject*)PyArray_FROM_OTF(arg1, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr1 == NULL) return NULL;
-    arr2 = (PyArrayObject*)PyArray_FROM_OTF(arg2, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr2 == NULL) goto fail;
     oarr = (PyArrayObject*)PyArray_FROM_OTF(out, NPY_DOUBLE, NPY_INOUT_ARRAY);
     if (oarr == NULL) goto fail;
 
     /*vv* code that makes use of arguments *vv*/
-
-    int nd = PyArray_NDIM(arr1);   //number of dimensions
-    npy_intp *shape = PyArray_DIMS(arr1);  // npy_intp array of length nd showing length in each dim.
-    for (int i=0; i<nd; ++i)
-        printf("%d ", shape[i]);
-    printf("\n");
-
-    for (int i=0; i<arr2->nd; ++i)
-        printf("%d ", arr2->dimensions[i]);
-    printf("\n");
 
     for (int i=0; i<oarr->dimensions[0]; ++i) {
         for (int j=0; j<oarr->dimensions[1]; ++j) {
@@ -38,21 +21,17 @@ static PyObject* example (PyObject *dummy, PyObject *args)
     }
     /*^^* code that makes use of arguments *^^*/
 
-    Py_DECREF(arr1);
-    Py_DECREF(arr2);
     Py_DECREF(oarr);
     Py_INCREF(Py_None);
     return Py_None;
 
 fail:
-    Py_XDECREF(arr1);
-    Py_XDECREF(arr2);
     PyArray_XDECREF_ERR(oarr);
     return NULL;
 }
 
 static struct PyMethodDef methods[] = {
-    {"example", example, METH_VARARGS, "descript of example"},
+    {"acquire", acquire, METH_VARARGS, NULL},
     {NULL, NULL, 0, NULL}
 };
 
