183cade5d1a4a2d89f91f6838fd19498059e24c4
simondlevy <simon.d.levy@gmail.com>
2016-09-30 15:16:14 -0400

    Added display title

--- opencv/python/capture.py
+++ opencv/python/capture.py
@@ -21,10 +21,10 @@ along with M021_V4L2.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import cv2
-from m021v4l2 import Capture1280x720
+from m021v4l2 import Capture800x460
 from time import time
 
-cap = Capture1280x720()
+cap = Capture800x460()
 
 start = time()
 
@@ -34,7 +34,7 @@ while True:
     ret, frame = cap.read()
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('LI-USB30-M021',frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
